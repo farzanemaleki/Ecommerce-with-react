35cdc4636a32e7874f8b410cc00f990f1951a557
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-03-01 13:59:16 +0330

    created necessary folders and install react-route and react-router-dom and define simple route in AppRoutes component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Header from './components/header/Header';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./components/routes/AppRoutes";
 
 function App() {
   return (
     <>
-    <Header />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+    <h1>farzaneh you can do it</h1>
+    </>
+  )
+}
+
+export default HomePage;
